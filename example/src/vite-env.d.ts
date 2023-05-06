7f7d20d608730dc70b17d3c5248bb4ebca356560
Johannes Zillmann <jzillmann@googlemail.com>
2023-05-06 12:05:46 -0600

    Fix check error of example project

--- example/src/vite-env.d.ts
+++ example/src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare namespace svelte.JSX {
+  interface HTMLProps<T> {
+    onclickedOutside?: (e: CustomEvent) => void;
+    oncontextMenuedOutside?: (e: CustomEvent) => void;
+  }
+}
